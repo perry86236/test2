37aeb159ae6f79e2fa7bcc7e7a5d8a43f7c77ca7
perry86236 <perry86236@gmail.com>
2019-03-12 06:40:24 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template, make_response
+from flask_bootstrap import Bootstrap
+import os
+import uuid
+import base64
+from PIL import Image
+import warnings
+warnings.simplefilter('error', Image.DecompressionBombWarning)
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+@app.route('/')
+def do_get():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
